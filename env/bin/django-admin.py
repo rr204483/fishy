5747959a4d1127b5c0d761f1c7d86750a108e71f
rr204483 <rramesh.cit@gmail.com>
2016-04-27 18:16:33 +0100

    initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/dj/projects/s2h/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ env/include/python2.7/pyconfig.h
@@ -0,0 +1,5 @@
+#ifdef _LP64
+#include <python2.7/pyconfig-64.h>
+#else
+#include <python2.7/pyconfig-32.h>
+#endif
