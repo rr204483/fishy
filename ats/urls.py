f5f9ac203936cd41ecb5d4d211032fa50f32d64e
koolkeerthi <koolkeerthi@gmail.com>
2016-06-06 17:33:14 +0100

    Forms control and methods to display

--- ats/urls.py
+++ ats/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^myview/$', views.curr_datetime),
-    url(r'^schooladd/$', views.create_school, name="school"),
+    url(r'^schooladd/$', views.create_school, name="schooladd"),
+    url(r'^listschools/$', views.list_schools, name="list_schools"),
+	url(r'^school/(?P<pk>\d+)/$', views.school_detail, name='school_detail'),
+	url(r'^school/(?P<pk>\d+)/edit/$', views.school_edit, name='school_edit'),
 ]
--- ats/views.py
+++ ats/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from ats.forms import SchoolForm
+from utils.models import School
+from django.shortcuts import render, get_object_or_404
 
 import datetime
 
@@ -15,9 +17,9 @@ def create_school(request):
     if request.method == 'POST':
         form = SchoolForm(request.POST)
         if form.is_valid():
-			form.save()
-			message = "School added"
-			return HttpResponseRedirect('.')
+            form.save()
+			#message = "School added"
+            return render(request, 'school_added.html', {'form': form})
     else:
         form = SchoolForm()
     context_data = {'form': form}
@@ -26,3 +28,30 @@ def create_school(request):
     # return render_to_response('create_school.html', context_data,
     #       context_instance=RequestContext(request))
     return render(request, 'create_school.html', context_data)
+	
+		
+def list_schools(request):
+    schools = School.objects.all()  
+    return render(request, 'list_schools.html', {'schools': schools})
+	
+def school_detail(request, pk):
+    school = get_object_or_404(School, pk=pk)
+    return render(request, 'school_detail.html', {'school': school})
+	
+def school_edit(request, pk):
+    school = get_object_or_404(School, pk=pk)
+    if request.method == 'POST':
+        form = SchoolForm(request.POST, instance=school)
+        if form.is_valid():
+            form.save()
+			#message = "School added"
+            return render(request, 'school_added.html', {'form': form})
+    else:
+        form = SchoolForm(instance=school)
+    context_data = {'form': form}
+
+    # return HttpResponse('create_school.html', context_data)
+    # return render_to_response('create_school.html', context_data,
+    #       context_instance=RequestContext(request))
+    return render(request, 'create_school.html', context_data)
+
